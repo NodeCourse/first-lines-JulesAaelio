9ea021c3b379b485cec62ade7fdbbaf43f55f3be
Jules LAURENT <jules.laurent@ynov.com>
2018-05-14 09:48:26 +0200

    loop, log live and log in file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
 console.log('Salut');
+
+function logTime() {
+    let timestamp = new Date().getTime().toString();
+    console.log('Saved : ', timestamp);
+
+    fs.appendFile('timelog.txt',  + timestamp +  '\n', (e) => {
+        if (e) throw e;
+    });
+}
+setInterval(logTime,1000);
